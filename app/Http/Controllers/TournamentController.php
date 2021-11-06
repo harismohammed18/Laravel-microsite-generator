b8f3454d6d0e469937576764efb7c6d3bb176e62
haris.mohammed <haris.mohammed@6dtech.co.in>
2021-11-07 01:01:12 +0530

    removed unwanted import

--- app/Http/Controllers/TournamentController.php
+++ app/Http/Controllers/TournamentController.php
@@ -5,10 +5,8 @@ namespace App\Http\Controllers;
 use App\Http\Requests\StoreTournamentRequest;
 use App\Http\Requests\UpdateTournamentRequest;
 use App\Models\Tournament;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Facades\Storage;
 use Illuminate\Validation\ValidationException;
 use ZipArchive;
 
